2b177442b5ccd786a3c0279495c9d96eb03d0034
JT1974 <takacsjan@gmail.com>
2022-07-14 16:50:09 +0200

    project set up with React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
 function App() {
+	/*
+		Your users should be able to:
+
+		See all countries from the API on the homepage
+		Search for a country using an input field
+		Filter countries by region
+		Click on a country to see more detailed information on a separate page
+		Click through to the border countries on the detail page
+		Toggle the color scheme between light and dark mode (optional)
+
+
+		Icons
+		Moon outline: <ion-icon name="moon-outline"></ion-icon>
+		Moon filled: <ion-icon name="moon"></ion-icon>
+		Search: <ion-icon name="search"></ion-icon>
+		Arrow back: <ion-icon name="arrow-back"></ion-icon>
+		Chevron down: <ion-icon name="chevron-down"></ion-icon>
+		
+	*/
 	return <div className='app'></div>
 }
 
